15e0902a003ecb71acc245eda4de372682e0fbd7
jcastle <jcastle@spreatics.com>
2024-07-04 13:30:47 +0900

    update

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class_asldfjsd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Class_asdjkfsdesf
+{
+    john.School school2;
+    codingon.School school1;
+
+    //School sc = new School();
+}
